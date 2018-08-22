1b61e13538714785100d1cda4f848f2c4bb5edb4
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-22 11:38:29 -0500

    added services for user and officer requests

--- Ksu.Gdc.Api.Core/Contracts/Services/IOfficerService.cs
+++ Ksu.Gdc.Api.Core/Contracts/Services/IOfficerService.cs
@@ -15,26 +15,26 @@ namespace Ksu.Gdc.Api.Core.Contracts
         /// Gets the officers.
         /// </summary>
         /// <returns>The officers.</returns>
-        List<Officer> GetOfficers();
+        List<IOfficer<IUser>> GetOfficers();
 
         /// <summary>
         /// Gets the officers async.
         /// </summary>
         /// <returns>The officers async.</returns>
-        Task<List<Officer>> GetOfficersAsync();
+        Task<List<IOfficer<IUser>>> GetOfficersAsync();
 
         /// <summary>
         /// Gets the officers by position.
         /// </summary>
         /// <returns>The officers by position.</returns>
         /// <param name="position">Position.</param>
-        List<Officer> GetOfficersByPosition(string position);
+        List<IOfficer<IUser>> GetOfficersByPosition(string position);
 
         /// <summary>
         /// Gets the officers by position async.
         /// </summary>
         /// <returns>The officers by position async.</returns>
         /// <param name="position">Position.</param>
-        Task<List<Officer>> GetOfficersByPositionAsync(string position);
+        Task<List<IOfficer<IUser>>> GetOfficersByPositionAsync(string position);
     }
 }
--- Ksu.Gdc.Api.Core/Contracts/Services/IUserService.cs
+++ Ksu.Gdc.Api.Core/Contracts/Services/IUserService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-using Ksu.Gdc.Api.Core.Models;
-
 namespace Ksu.Gdc.Api.Core.Contracts
 {
     /// <summary>
@@ -15,27 +13,27 @@ namespace Ksu.Gdc.Api.Core.Contracts
         /// </summary>
         /// <returns>The user by identifier.</returns>
         /// <param name="id">Identifier.</param>
-        User GetUserById(int id);
+        IUser GetUserById(int id);
 
         /// <summary>
         /// Gets the user by identifier async.
         /// </summary>
         /// <returns>The user by identifier async.</returns>
         /// <param name="id">Identifier.</param>
-        Task<User> GetUserByIdAsync(int id);
+        Task<IUser> GetUserByIdAsync(int id);
 
         /// <summary>
         /// Gets the user by username.
         /// </summary>
         /// <returns>The user by username.</returns>
         /// <param name="username">Username.</param>
-        User GetUserByUsername(string username);
+        IUser GetUserByUsername(string username);
 
         /// <summary>
         /// Gets the user by username async.
         /// </summary>
         /// <returns>The user by username async.</returns>
         /// <param name="username">Username.</param>
-        Task<User> GetUserByUsernameAsync(string username);
+        Task<IUser> GetUserByUsernameAsync(string username);
     }
 }
