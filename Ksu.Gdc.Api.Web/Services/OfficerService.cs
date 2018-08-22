c0942f50705a122eae1d56c7516b3ebca47c0541
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-22 11:41:40 -0500

    added user and officer services

--- Ksu.Gdc.Api.Web/Services/OfficerService.cs
+++ Ksu.Gdc.Api.Web/Services/OfficerService.cs
@@ -1,10 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Ksu.Gdc.Api.Core.Contracts;
+using Ksu.Gdc.Api.Core.Models;
+
 namespace Ksu.Gdc.Api.Web.Services
 {
-    public class OfficerService
+    public class OfficerService : IOfficerService
     {
-        public OfficerService()
+        public List<IOfficer<IUser>> GetOfficers()
         {
+            return GetOfficersAsync().Result;
+        }
+
+        public async Task<List<IOfficer<IUser>>> GetOfficersAsync()
+        {
+
+        }
+
+        public List<IOfficer<IUser>> GetOfficersByPosition(string position)
+        {
+            return GetOfficersByPositionAsync(position).Result;
+        }
+
+        public async Task<List<IOfficer<IUser>>> GetOfficersByPositionAsync(string position)
+        {
+
         }
     }
 }
--- Ksu.Gdc.Api.Web/Services/UserService.cs
+++ Ksu.Gdc.Api.Web/Services/UserService.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Ksu.Gdc.Api.Core.Contracts;
+
 namespace Ksu.Gdc.Api.Web.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
-        public UserService()
+        public IUser GetUserById(int id)
         {
+            return GetUserByIdAsync(id).Result;
+        }
+
+        public async Task<IUser> GetUserByIdAsync(int id)
+        {
+
+        }
+
+        public IUser GetUserByUsername(string username)
+        {
+            return GetUserByUsernameAsync(username).Result;
+        }
+
+        public async Task<IUser> GetUserByUsernameAsync(string username)
+        {
+
         }
     }
 }
