226c8ff3dc8f9c6296b5e53d5e577c8226d5fd03
Dayton Taylor <daytonltaylor@outlook.com>
2018-08-25 23:37:15 -0500

    modified games image folder path

--- Ksu.Gdc.Api.Core/Configurations/GameConfig.cs
+++ Ksu.Gdc.Api.Core/Configurations/GameConfig.cs
@@ -4,7 +4,7 @@ namespace Ksu.Gdc.Api.Core.Configurations
 {
     public static class GameConfig
     {
-        public static string DataStoreUrl = AppConfiguration.GetConfig("DataStoreUrl") + "/games";
+        public static string DataStoreUrl = AppConfiguration.GetConfig("DataStoreUrl") + "/portfolio/games";
 
         public static int MaxImageCount = 4;
     }
