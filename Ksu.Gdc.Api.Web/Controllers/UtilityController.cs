209f3713c0f7d4366091dba1b4ca2b9669f5a760
Dayton Taylor <daytonltaylor@outlook.com>
2018-09-05 10:13:12 -0500

    added stay awake request

---
+++ Ksu.Gdc.Api.Web/Controllers/UtilityController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ksu.Gdc.Api.Web.Controllers
+{
+    [Route("")]
+    public class UtilityController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult KeepAwake()
+        {
+            return Ok("API is awake!");
+        }
+    }
+}
