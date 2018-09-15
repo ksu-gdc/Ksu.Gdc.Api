5dfe9bec1022e30b99296fc81240818bb4b7af9d
Dayton Taylor <daytonltaylor@outlook.com>
2018-09-15 13:21:52 -0500

    added try catch

--- Ksu.Gdc.Api.Web/Controllers/UtilityController.cs
+++ Ksu.Gdc.Api.Web/Controllers/UtilityController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,7 +16,14 @@ namespace Ksu.Gdc.Api.Web.Controllers
         [Route("")]
         public IActionResult KeepAwake()
         {
-            return Ok("API is awake!");
+            try
+            {
+                return Ok("API is awake!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
     }
 }
